295f5a5d7ac1b965d0fc38a1b009e1eb5b61bf70
Puniaharsh <200361570@student.georgianc.on.ca>
2018-02-13 16:57:18 -0500

    first commit

---
+++ Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(string product)
+        {
+            ViewBag.product = product;
+            ViewBag.Message = "You selected product: " + product;
+            return View();
+        }
+    }
+}
